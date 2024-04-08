7749d94e2088ee6a1495f23503cd0785ee83bf9f
Aarni Junkkala <aarni.junkkala@gmail.com>
2024-04-08 16:45:26 +0300

    Update
    Update

--- ImageEdit/Program.cs
+++ ImageEdit/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Net.Http.Headers;
 
 namespace ImageEdit
 {
@@ -13,7 +8,8 @@ namespace ImageEdit
         static void Main(string[] args)
         {
             ImageOpener ImgO = new ImageOpener();
-            Image I = ImgO.OpenImage();
+            Image I = ImgO.OpenImage("C:\\Users\\aarnjunk\\Documents\\Big-O.png");
+            Console.ReadLine();
         }
     }
 
@@ -49,20 +45,79 @@ namespace ImageEdit
         }
     }
 
+    internal class Chunk
+    {
+        byte[] ChunkType = new byte[4];
+        byte[] ChunkData = new byte[0];
+        byte[] CRC = new byte[4];
+        public Chunk(byte[] Data)
+        {
+            Array.Copy(Data, 0, ChunkType, 0, 4);
+            ChunkData = new byte[Data.Length - 8]; //Resizes the ChunkData
+            Array.Copy(Data, 4, ChunkData, 0, Data.Length - 8);
+            Array.Copy(Data, Data.Length - 4 - 1, CRC, 0, 4);
+            //Debugging
+            Console.WriteLine(ByteToASCII(ChunkType));
+            //Console.WriteLine(ChunkData);
+            //Console.WriteLine(CRC);
+        }
+
+        public string ByteToASCII(byte[] data)
+        {
+            return System.Text.Encoding.ASCII.GetString(data);
+        }
+    }
+
     internal class ImageOpener
     {
         public byte[] Bytes;
-        public Image OpenImage()
+        public Image OpenImage(string path)
         {
-            byte[] Bytes = System.IO.File.ReadAllBytes("C:\\Users\\aarnjunk\\Documents\\Omena.png");
+            byte[] Bytes = System.IO.File.ReadAllBytes(path);
 
-            byte[] Part = new byte[8];
-            for (int i = 0; i < 8; i++)
-            {
-                Part[i] = Bytes[i];
+            byte[] Header = new byte[8];
+            Array.Copy(Bytes, 0, Header, 0, 8);
+
+            Console.WriteLine("Is png:" + IsPNG(Header));
+            if(IsPNG(Header) == false){
+                return null;
             }
-            Console.WriteLine(IsPNG(Part));
-            return new Image();
+
+            Chunk[] Chunks = new Chunk[0];
+            int ChunkIndex = 8;
+            while(true){
+                byte[] ChunkLength = new byte[4];
+                Array.Copy(Bytes, ChunkIndex, ChunkLength, 0, 4);
+                int ChunkLengthInt = BytesToDecimal(ChunkLength);
+                byte[] ChunkData = new byte[ChunkLengthInt + 8];
+                Array.Copy(Bytes, ChunkIndex + 4, ChunkData, 0, ChunkLengthInt + 8);
+                
+                //Lengthens the array by one. We don't know how many chunks will be in the file.
+                //TODO: determine how many chunks are there, before checking each of them.
+                Chunk[] Holder = new Chunk[Chunks.Length];
+                Array.Copy(Chunks,0,Holder,0,Chunks.Length);
+                Chunks = new Chunk[Chunks.Length + 1];
+                Array.Copy(Holder,0,Chunks,0,Holder.Length);
+
+                Chunks[Chunks.Length - 1] = new Chunk(ChunkData);
+                ChunkIndex += ChunkLengthInt + 12;
+                if(ChunkIndex >= Bytes.Length){
+                    break;
+                }
+            }
+
+            Image I = new Image();
+            
+            return I;
+        }
+
+        public int BytesToDecimal(byte[] data){
+            int number = 0;
+            for(int i = 0; i < data.Length; i++){
+                number += (int)(Math.Pow(2, (data.Length - 1 - i) * 8) * data[i]);
+            }
+
+            return number;
         }
 
         public bool IsPNG(byte[] Header)
